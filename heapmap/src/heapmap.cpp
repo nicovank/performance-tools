a7a0b7a572e6445cd48142974327e66fca23b3ca
Nicolas van Kempen <nvankempen@gmail.com>
2021-09-30 14:48:22 -0400

    Fix possible multi-threading safety issue

--- heapmap/src/heapmap.cpp
+++ heapmap/src/heapmap.cpp
@@ -12,16 +12,38 @@
 static const INT64 PARTITION_SIZE = 1 << LG_PARTITION_SIZE;
 
 using BinMap = std::unordered_map<ADDRINT, unsigned long long>;
-static std::unordered_map<THREADID, BinMap> Accesses;
+static BinMap Accesses;
+static PIN_MUTEX AccessesLock;
+static TLS_KEY TlsKey = INVALID_TLS_KEY;
 
 static std::ofstream OutputFile;
 static KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "heapmap.out", "specify output file name");
 
+VOID OnThreadStart(THREADID ThreadId, CONTEXT* Context, INT32 Flags, VOID* _) {
+  BinMap* ThreadData = new BinMap;
+  // Performance improvement: reserve the BinMap with an estimate of how many bins will be needed.
+  if (PIN_SetThreadData(TlsKey, ThreadData, ThreadId) == false) {
+    std::cerr << "Failed PIN_SetThreadData." << std::endl;
+    PIN_ExitProcess(1);
+  }
+}
+
+VOID OnThreadEnd(THREADID ThreadId, const CONTEXT* Context, INT32 Code, VOID* _) {
+  BinMap* ThreadData = static_cast<BinMap*>(PIN_GetThreadData(TlsKey, ThreadId));
+  PIN_MutexLock(&AccessesLock);
+  for (std::pair<ADDRINT, unsigned long long> Counter : *ThreadData) {
+    Accesses[Counter.first] += Counter.second;
+  }
+  PIN_MutexUnlock(&AccessesLock);
+  delete ThreadData;
+}
+
 inline ATTRIBUTE_INLINE VOID OnMemoryAccess(THREADID ThreadId, ADDRINT Pointer, UINT32 Size) {
+  BinMap* ThreadData = static_cast<BinMap*>(PIN_GetThreadData(TlsKey, ThreadId));
   ADDRINT MinBin = Pointer >> LG_PARTITION_SIZE;
   ADDRINT MaxBin = (Pointer + Size) >> LG_PARTITION_SIZE;
   for (ADDRINT Bin = MinBin; Bin <= MaxBin; ++Bin) {
-    ++Accesses[ThreadId][Bin];
+    ++ThreadData->operator[](Bin);
   }
 }
 
@@ -33,15 +55,9 @@ VOID OnProgramEnd(INT32 Code, VOID* _) {
   OutputFile << '\t' << "\"LG_PARTITION_SIZE\": " << LG_PARTITION_SIZE << "," << std::endl;
   OutputFile << '\t' << "\"Bins\": [";
 
-  BinMap Totals;
-  for (std::pair<THREADID, BinMap> PerThreadCounters : Accesses) {
-    for (std::pair<ADDRINT, unsigned long long> Counter : PerThreadCounters.second) {
-      Totals[Counter.first] += Counter.second;
-    }
-  }
-
-  if (!Totals.empty()) {
-    std::vector<std::pair<ADDRINT, unsigned long long>> Counters(Totals.begin(), Totals.end());
+  PIN_MutexLock(&AccessesLock);
+  if (!Accesses.empty()) {
+    std::vector<std::pair<ADDRINT, unsigned long long>> Counters(Accesses.begin(), Accesses.end());
     std::sort(Counters.begin(), Counters.end(), [](auto a, auto b) { return a.first < b.first; });
 
     OutputFile << Counters[0].second;
@@ -49,6 +65,7 @@ VOID OnProgramEnd(INT32 Code, VOID* _) {
       OutputFile << ", " << Counters[i].second;
     }
   }
+  PIN_MutexUnlock(&AccessesLock);
 
   OutputFile << "]" << std::endl << "}" << std::endl;
 }
@@ -79,7 +96,19 @@ int main(int argc, char* argv[]) {
 
   OutputFile.open(KnobOutputFile.Value().c_str());
 
+  if ((TlsKey = PIN_CreateThreadDataKey(nullptr)) == INVALID_TLS_KEY) {
+    std::cerr << "Failed PIN_CreateThreadDataKey." << std::endl;
+    PIN_ExitProcess(1);
+  }
+
+  if (!PIN_MutexInit(&AccessesLock)) {
+    std::cerr << "Failed PIN_MutexInit." << std::endl;
+    PIN_ExitProcess(1);
+  }
+
   INS_AddInstrumentFunction(Instruction, nullptr);
+  PIN_AddThreadStartFunction(OnThreadStart, nullptr);
+  PIN_AddThreadFiniFunction(OnThreadEnd, nullptr);
   PIN_AddFiniFunction(OnProgramEnd, nullptr);
 
   PIN_StartProgram();
